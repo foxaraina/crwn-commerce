05b4ade61351b134222ff90233d7bd5cf3bdc97a
foxaraina <dovzhik.a@gmail.com>
2024-06-25 20:41:00 +0300

    added home page links

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {BackgroundImage, CategoryBody, CategoryContainer} from "./category-item.styles";
+import {useNavigate} from "react-router-dom";
 
 const CategoryItem = ({category}) => {
-    const {imageUrl, title } = category
+    const {imageUrl, title, route } = category
+    const navigate = useNavigate();
     return (
-        <CategoryContainer>
+        <CategoryContainer onClick={() => navigate(route)}>
             <BackgroundImage imageUrl={imageUrl}  />
             <CategoryBody>
                 <h2>{title}</h2>
